48e608cb19d403223d714c92694209f523e7637d
pnichols789 <pnichols789@gmail.com>
2022-11-03 09:24:05 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import MyVerticallyCenteredModal from "./components/Modal";
 
-function App() {
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [list, setListValue] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setListValue([...list, inputValue]);
+    setInputValue("");
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        style={{ width: "15rem" }}
+        className="mx-auto mt-5"
+        onSubmit={handleSubmit}
+      >
+        <Form.Control
+          className="my-3"
+          type="text"
+          placeholder="enter any text"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <Button type="submit">Submit</Button>
+      </Form>
+      <Container className="mt-4">
+        {list.map((item, index) => (
+          <>
+            <Row
+              className="my-3"
+              key={index}
+              onClick={() => setModalShow(true)}
+            >
+              <Col>{item.length > 5 ? item.slice(0, 5) + "..." : item}</Col>
+              <MyVerticallyCenteredModal
+                item={item}
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
+            </Row>
+          </>
+        ))}
+      </Container>
     </div>
   );
 }
-
-export default App;
